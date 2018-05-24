f82eefeba4f065489a032b94ae90ada3c73d0abc
Daniil Dulin <daniildulin@gmail.com>
2018-05-24 13:53:44 +0300

    fix kernal import

--- service/app/Console/Kernel.php
+++ service/app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use App\Console\Commands\FillTxPerDayCountTableCommand;
+use App\Console\Commands\FillTxPerDayTableCommand;
 use App\Console\Commands\PullBlockDataCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         PullBlockDataCommand::class,
-        FillTxPerDayCountTableCommand::class,
+        FillTxPerDayTableCommand::class,
     ];
 
     /**
