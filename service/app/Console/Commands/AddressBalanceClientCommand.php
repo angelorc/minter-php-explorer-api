a5a0e935c257d21f299261df9efe592fe9d4601d
Daniil Dulin <daniildulin@gmail.com>
2018-08-21 16:10:29 +0300

    balance listener rework

--- service/app/Console/Commands/AddressBalanceClientCommand.php
+++ service/app/Console/Commands/AddressBalanceClientCommand.php
@@ -2,12 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Helpers\StringHelper;
 use App\Models\Balance;
 use App\Models\BalanceChannel;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
-use Wrench\Client;
+use Wrench\Client as wsClient;
 use Wrench\Exception\FrameException;
 use Wrench\Exception\HandshakeException;
 use Wrench\Exception\SocketException;
@@ -32,16 +34,22 @@ class AddressBalanceClientCommand extends Command
      */
     protected $centrifuge;
 
+    /**
+     * @var Client
+     */
+    protected $httpClient;
 
     /**
      * AddressBalanceClientCommand constructor.
      *
      * @param \phpcent\Client $centrifuge
+     * @param Client $httpClient
      */
-    public function __construct(\phpcent\Client $centrifuge)
+    public function __construct(\phpcent\Client $centrifuge, Client $httpClient)
     {
         parent::__construct();
         $this->centrifuge = $centrifuge;
+        $this->httpClient = $httpClient;
     }
 
 
@@ -49,7 +57,7 @@ class AddressBalanceClientCommand extends Command
     {
         try {
 
-            $client = new Client('ws://' . env('MINTER_API') . $this::ENDPOINT,
+            $client = new wsClient('ws://' . env('MINTER_API') . $this::ENDPOINT,
                 'http://' . env('MINTER_API') . $this::ENDPOINT);
             $client->connect();
             $response = true;
@@ -60,44 +68,24 @@ class AddressBalanceClientCommand extends Command
                 if (is_null($response)) {
                     break;
                 }
-                $data     = null;
-                $channels = [];
+
+                $data = null;
+                $addresses = [];
 
                 foreach ($response as $r) {
-                    $data    = \GuzzleHttp\json_decode($r->getPayload());
-                    $balance = Balance::updateOrCreate(
-                        ['address' => mb_strtolower($data->address), 'coin' => mb_strtoupper($data->coin)],
-                        ['amount' => $data->balance]
-                    );
-
-                    /** @var Collection $bl */
-                    $balanceChannelList = BalanceChannel::where('address', mb_strtolower($data->address))->get();
-
-                    foreach ($balanceChannelList as $balanceChannel) {
-                        /** BalanceChannel $balanceChannel */
-                        $channels[$balanceChannel->name][] = $balance;
-                    }
-
+                    $data = \GuzzleHttp\json_decode($r->getPayload());
+                    $addresses[] = mb_strtolower($data->address);
                 }
 
-                if (\count($channels)) {
-                    foreach ($channels as $name => $balances) {
-                        foreach ($balances as $balance) {
-                            $this->centrifuge->publish($name, [
-                                'address' => mb_strtolower($balance->address),
-                                'coin'    => mb_strtoupper($balance->coin),
-                                'amount'  => $balance->amount
-                            ]);
-                        }
-
-                    }
+                $addresses = array_unique($addresses);
+                if (\count($addresses)) {
+                    $this->updateAddressesBalance($addresses);
                 }
 
                 // при подключении первым приходит пустой массив, поэтому проверяем
                 if (!\count($response) || !isset($data->disconect)) {
                     $response = true;
                 }
-
             }
 
             $client->disconnect();
@@ -111,5 +99,54 @@ class AddressBalanceClientCommand extends Command
                 $exception->getMessage()
             );
         }
+    }
+
+    private function updateAddressesBalance(array $addresses)
+    {
+        $channels = [];
+
+        foreach ($addresses as $address) {
+            $res = $this->httpClient->request('GET', 'api/balance/' . StringHelper::mb_ucfirst($address));
+            $data = json_decode($res->getBody()->getContents(), 1);
+
+            Balance::where('address', 'ilike', $address)->delete();
+
+            /** @var Collection $bl */
+            $balanceChannelList = BalanceChannel::where('address', 'ilike', $address)->get();
+
+            foreach ($data["result"]["balance"] as $coin => $value) {
+                $balance = new Balance;
+                $balance->address = mb_strtolower($address);
+                $balance->coin = mb_strtoupper($coin);
+                $balance->amount = $value;
+                $balance->save();
+
+                foreach ($balanceChannelList as $balanceChannel) {
+                    /** BalanceChannel $balanceChannel */
+                    $channels[$balanceChannel->name][] = $balance;
+                }
+            }
+        }
+
+        if (\count($channels)) {
+            foreach ($channels as $name => $balances) {
+                foreach ($balances as $balance) {
+                    $this->centrifuge->publish($name, [
+                        'address' => mb_strtolower($balance->address),
+                        'coin' => mb_strtoupper($balance->coin),
+                        'amount' => $balance->amount
+                    ]);
+                }
+            }
+        }
+
+        $this->clearOldChannels();
+    }
+
+    private function clearOldChannels()
+    {
+        $dt = new \DateTime();
+        $dt->modify('-10 day');
+        BalanceChannel::whereDate('created_at', '<=', $dt->format('Y-m-d H:i:s'));
     }
 }
--- service/app/Models/Balance.php
+++ service/app/Models/Balance.php
@@ -4,6 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Class Balance
+ * @package App\Models
+ *
+ * @property string address
+ * @property string coin
+ * @property string amount
+ */
 class Balance extends Model
 {
     protected $dateFormat = 'Y-m-d H:i:sO';
