48bc835754861682e9efa871b9a7030ef0ca9211
Daniil Dulin <daniildulin@gmail.com>
2018-08-27 14:35:01 +0300

    disable update balance by crone

--- service/app/Console/Kernel.php
+++ service/app/Console/Kernel.php
@@ -36,6 +36,5 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('transactions:per_count_save')->dailyAt('00:01');
-        $schedule->command('balance:update_data')->everyMinute(); //TODO: убрать как разберешься с балансом
     }
 }
