dde2f70f1a6c8ea8423e875a898864c21a08d752
Daniil Dulin <daniildulin@gmail.com>
2018-09-07 11:38:47 +0300

    fix tx-count-chart-data

--- service/routes/api/v1.php
+++ service/routes/api/v1.php
@@ -6,7 +6,7 @@ $router->get('/', function ()    {
 
 $router->get('status', StatusController::class. '@status');
 $router->get('txCountChartData', StatusController::class . '@txCountChartData'); //Для совместимости со старыми версиями
-$router->get('tx-count-chart-cata', StatusController::class . '@txCountChartData');
+$router->get('tx-count-chart-data', StatusController::class . '@txCountChartData');
 $router->get('status_page', StatusController::class . '@statusPage'); //Для совместимости со старыми версиями
 $router->get('status-page', StatusController::class . '@statusPage');
 $router->get('get-actual-node', StatusController::class . '@getActualNode');
